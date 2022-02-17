6f2cec8b0640f2b5b03f9987d57c2ee0c33e6c45
Canelle Deguitre <cdeguitre@outlook.fr>
2022-02-17 16:52:01 +0100

    done for back

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,5 +1,6 @@
 class Movie < ApplicationRecord
   has_many :bookmarks
+  has_many :list, through: :bookmarks
   validates :title, uniqueness: true
   validates :title, presence: true
   validates :overview, presence: true
